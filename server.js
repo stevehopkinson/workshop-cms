bf2125089e99b00ab46b5330e8cbc876ef3f4aac
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-11-07 15:22:36 +0000

    Import fs and serve index.html

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 var http = require('http');
+var fs = require('fs');
 
 var port = 3000;
 
 var server = http.createServer(handler);
 
-var message = "I am so happy to be part of the Node Girls workshop.";
+var message = "Code on, code on, with node in your hearts, and you'll never code alone.";
+var nodeMessage = "Node on!";
+var girlsMessage = "Go girls!";
 
 function handler (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(message);
-  response.end();
+  var endpoint = request.url;
+  var method = request.method;
+  console.log("Endpoint: " + endpoint + ", Method: " + method);
+  if (endpoint === "/") {
+    response.writeHead(200, {"Content-Type": "text/html"});
+
+    fs.readFile(__dirname + '/public/index.html', function (error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    })
+  }
 }
 
 server.listen(port, function () {
